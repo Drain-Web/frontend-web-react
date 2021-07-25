eb58b1b447c58d9bf621849ac6f0b43270069f52
adlzanchetta <adlzanchetta@gmail.com>
2021-07-25 18:47:45 -0400

    Fix codding standards

--- client/src/components/BaseLayers.js
+++ client/src/components/BaseLayers.js
@@ -1,46 +1,36 @@
-import { TileLayer, LayersControl } from "react-leaflet";
-import React from 'react';
+import { TileLayer, LayersControl } from 'react-leaflet'
+import React from 'react'
 
-
-const { BaseLayer } = LayersControl;
-
+const { BaseLayer } = LayersControl
 
 const BaseLayers = ({ baseLayerData }) => {
+  return (
+    <>
+      {baseLayerData.map((baseLayer) => {
+        if (baseLayer.maxNativeZoom) {
+          return (
+            <BaseLayer checked={baseLayer.checked} name={baseLayer.name} key={baseLayer.name}>
+              <TileLayer
+                attribution={baseLayer.attribution}
+                url={baseLayer.url}
+                maxNativeZoom={baseLayer.maxNativeZoom}
+              />
+            </BaseLayer>
+          )
+        } else {
+          return (
+            <BaseLayer checked={baseLayer.checked} name={baseLayer.name} key={baseLayer.name}>
+              <TileLayer
+                attribution={baseLayer.attribution}
+                url={baseLayer.url}
+              />
+            </BaseLayer>
+          )
+        }
+      })}
+    </>
 
-    return (
-        <React.Fragment>
-            {baseLayerData.map((baseLayer) => {  
-                        
-                if (baseLayer.maxNativeZoom) {
-                    return(
-                        <BaseLayer checked={baseLayer.checked} name={baseLayer.name} key={baseLayer.name}>
-                            <TileLayer
-                                attribution={baseLayer.attribution}
-                                url={baseLayer.url}
-                                maxNativeZoom={baseLayer.maxNativeZoom}
-                            />
-                        </BaseLayer>
-                    );
-                } else {
-                    return(
-                        <BaseLayer checked={baseLayer.checked} name={baseLayer.name} key={baseLayer.name}>
-                            <TileLayer
-                                attribution={baseLayer.attribution}
-                                url={baseLayer.url}
-                            />
-                        </BaseLayer>
-                    );
-                }
-            })}
-        </React.Fragment>
-
-
-    );
+  )
 }
 
-export default BaseLayers;
-
-
-
-
-
+export default BaseLayers
