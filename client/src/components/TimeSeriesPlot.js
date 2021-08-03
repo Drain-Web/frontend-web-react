b0761b1d8251a08919bb7b61ab2a566295c63daa
Sebastian <seospina@gmail.com>
2021-08-03 14:05:28 -0500

    npx standard fix implemented to all scripts

--- client/src/components/TimeSeriesPlot.js
+++ client/src/components/TimeSeriesPlot.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 import {
   ResponsiveContainer,
   LineChart,
@@ -9,45 +9,43 @@ import {
   Tooltip,
   Line,
   Legend
-} from "recharts";
-import "../style/timeSeriePlot.css";
-import useSWR from "swr";
-import axios from "axios";
-
-
+} from 'recharts'
+import '../style/timeSeriePlot.css'
+import useSWR from 'swr'
+import axios from 'axios'
 
 const TimeSeriesPlot = ({ timeSeriesUrl }) => {
-  const [plotData, setPlotData] = useState(null);
-  const fetcher = (url) => axios.get(url).then((res) => res.data);
+  const [plotData, setPlotData] = useState(null)
+  const fetcher = (url) => axios.get(url).then((res) => res.data)
 
-  const { data: apiData, error: error } = useSWR(timeSeriesUrl, fetcher, {
-    suspense: true,
-  });
-  if (error) return <div>failed to load</div>;
-  if (!apiData) return <div>loading...</div>;
+  const { data: apiData, error } = useSWR(timeSeriesUrl, fetcher, {
+    suspense: true
+  })
+  if (error) return <div>failed to load</div>
+  if (!apiData) return <div>loading...</div>
 
-  let plotDataAux;
+  let plotDataAux
 
   const getPlotData = async () => {
-    setPlotData(null);
-    plotDataAux = await apiData[0]["events"].map((timeStep) => {
+    setPlotData(null)
+    plotDataAux = await apiData[0].events.map((timeStep) => {
       return {
-        date: parseInt((new Date(timeStep.date + " " + timeStep.time).getTime() / 1000).toFixed(0)),
-        value: timeStep.value,
-      };
-    });
-    setPlotData(plotDataAux);
-  };
+        date: parseInt((new Date(timeStep.date + ' ' + timeStep.time).getTime() / 1000).toFixed(0)),
+        value: timeStep.value
+      }
+    })
+    setPlotData(plotDataAux)
+  }
 
   // getPlotData();
-  useEffect(() => getPlotData(), []);
+  useEffect(() => getPlotData(), [])
 
-  console.log(plotData);
+  console.log(plotData)
 
   return (
     <>
       {plotData && (
-        <ResponsiveContainer width="90%" height="60%" className="grafica">
+        <ResponsiveContainer width='90%' height='60%' className='grafica'>
           <LineChart
             width={500}
             height={300}
@@ -56,39 +54,39 @@ const TimeSeriesPlot = ({ timeSeriesUrl }) => {
               top: 5,
               right: 30,
               left: 14,
-              bottom: 5,
+              bottom: 5
             }}
           >
-            <CartesianGrid strokeDasharray="3 3" />
-      <XAxis type="number" dataKey="date" domain={["dataMin", "dataMax"]}>
-        <Label
-          value={"Date"}
-          position="bottom"
-          style={{ textAnchor: "middle" }}
-        />
-      </XAxis>
-      <YAxis>
-        <Label
-          value={"Value"}
-          position="left"
-          angle={-90}
-          style={{ textAnchor: "middle" }}
-        />
-      </YAxis>
-      <Line dataKey="value" />
-            {/* <Tooltip /> */}
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis type='number' dataKey='date' domain={['dataMin', 'dataMax']}>
+              <Label
+                value='Date'
+                position='bottom'
+                style={{ textAnchor: 'middle' }}
+              />
+            </XAxis>
+            <YAxis>
+              <Label
+                value='Value'
+                position='left'
+                angle={-90}
+                style={{ textAnchor: 'middle' }}
+              />
+            </YAxis>
+            <Line dataKey='value' />
+            <Tooltip />
             <Legend />
             <LineChart
-              type="monotone"
-              dataKey="date"
-              stroke="#8884d8"
+              type='monotone'
+              dataKey='date'
+              stroke='#8884d8'
               dot={false}
             />
           </LineChart>
         </ResponsiveContainer>
       )}
     </>
-  );
-};
+  )
+}
 
-export default TimeSeriesPlot;
+export default TimeSeriesPlot
